ff839949b8c04404a498748a93678245306e2ed9
ChristineLei <christine2food@gmail.com>
2020-10-28 18:08:47 +0800

    Week02 homework okhttp and httpclient

---
+++ Week_02/homework/src/main/java/java0/httpclient/httpclient.java
@@ -0,0 +1,33 @@
+package java0.httpclient;
+
+import java.util.Scanner;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class httpclient {
+
+    public static void main(String args[]) throws Exception{
+
+        //Creating a HttpClient object
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //Creating a HttpGet object
+        HttpGet httpget = new HttpGet("http://localhost:8801/");
+
+        //Printing the method used
+        System.out.println("Request Type: "+httpget.getMethod());
+
+        //Executing the Get request
+        HttpResponse httpresponse = httpclient.execute(httpget);
+
+        Scanner sc = new Scanner(httpresponse.getEntity().getContent());
+
+        //Printing the status line
+        System.out.println(httpresponse.getStatusLine());
+        while(sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
